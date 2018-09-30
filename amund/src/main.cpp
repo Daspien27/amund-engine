93ce4c0ffbbb0679fe64054de377096585e1f3cf
Amund Desmarais <amunddesmarais64@gmail.com>
2018-09-30 08:21:20 -0400

    GLFW

--- amund/src/main.cpp
+++ amund/src/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
+#include <glfw/glfw3.h>
+
 int main(int, char**) {
+
+    glfwInit ();
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_VERSION_MINOR, 6);
+
+    auto window = glfwCreateWindow(800,800,"Hello World!", nullptr, nullptr);
+
+    if (!window)
+    {
+        std::cerr << "Error creating GLFW window!" << std::endl;
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents ();
+
+
+
+        glfwSwapBuffers (window);
+    }
+
     std::cout << "Hello, world!\n";
 }
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Amund/source/repos/amund-engine
-# Build directory: C:/Users/Amund/source/repos/amund-engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
