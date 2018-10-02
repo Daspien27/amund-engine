939149812167c085b237304342157f9aa78ffb48
Amund Desmarais <amunddesmarais64@gmail.com>
2018-10-02 09:10:52 -0400

    Uniform Buffer Set Up
    Doesn't rotate the triangle. But seems to read the uniform buffer (have to determine whether the correct data is passed in).

--- amund/assets/shaders/shader.vert
+++ amund/assets/shaders/shader.vert
@@ -1,14 +1,12 @@
 #version 460
 #extension GL_ARB_separate_shader_objects : enable
 
-/*
 layout (binding = 0) uniform UniformBufferObject 
 {
 	mat4 model;
 	mat4 view;
 	mat4 proj;
 } ubo;
-*/
 
 layout(location = 0) in vec3 inPosition;
 //layout(location = 1) in vec3 inColor;
@@ -24,7 +22,7 @@ out gl_PerVertex
 
 void main ()
 {
-	gl_Position = /*ubo.proj * ubo.view * ubo.model **/ vec4(inPosition, 1.0);
+	gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 1.0);
 
 	//fragColor = inColor;
 }
--- amund/src/main.cpp
+++ amund/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -6,6 +7,7 @@
 #include <reactphysics3d.h>
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "amund/shader.h"
 
@@ -37,15 +39,70 @@ int main(int, char**) {
 
     std::cout << "Properly initialized gl bindings." << std::endl;
 
+    glViewport (0, 0, 800, 800);
+
+    //Shader Program Data
     auto vs_loc = std::filesystem::current_path() / "build" / "assets" / "shaders" / "shader_vert.spv";
     auto fs_loc = std::filesystem::current_path() / "build" / "assets" / "shaders" / "shader_frag.spv";
 
     auto gp = amund::graphics::GraphicsShaderProgram (vs_loc, fs_loc);
 
+    //Uniform Data
     gp.use ();
+    auto proj = glm::ortho(-4, 4, -4, 4, 1, 100);
+    auto view = glm::lookAt(glm::vec3(0.0,0.0, -5.0), glm::vec3(0.0,0.0,0.0), glm::vec3(0.0,1.0,0.0));
 
-    glViewport (0, 0, 800, 800);
+    auto uniform = glGetUniformBlockIndex (1, "UniformBufferObject");
 
+    assert(uniform != GL_INVALID_INDEX);
+
+    unsigned int binding = 0;
+    glUniformBlockBinding(1, uniform, binding);
+
+    unsigned int ubo = 0;
+    glGenBuffers(1, &ubo);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo);
+ 
+    struct UniformBufferObject
+    {
+        glm::mat4 proj;
+        glm::mat4 view;
+        glm::mat4 model;
+
+        void BindBufferData(unsigned int binding, unsigned int buffer)
+        {
+            float uniform [16*3];
+
+            auto p_proj = glm::value_ptr(proj);
+            for (int i  = 0; i < 16; ++i)
+            {
+                uniform[i] = p_proj[i];
+            }
+
+            auto p_view = glm::value_ptr(view);
+            for (int i  = 0; i < 16; ++i)
+            {
+                uniform[i+16] = p_view[i];
+            }
+
+            auto p_model = glm::value_ptr(model);
+            for (int i  = 0; i < 16; ++i)
+            {
+                uniform[i+32] = p_model[i];
+            }
+
+            glBufferData(GL_UNIFORM_BUFFER, sizeof(uniform), uniform, GL_DYNAMIC_DRAW);
+            glBindBufferBase(GL_UNIFORM_BUFFER, binding, buffer);
+        }
+    } MyUniform;
+
+    MyUniform.proj = proj;
+    MyUniform.view = view;
+
+    MyUniform.BindBufferData(binding, ubo);
+
+
+    //Vertex Data
     std::vector<float> triangle = {-0.5f, -0.5f, 0.0f,
                                     0.5f, -0.5f, 0.0f,
                                     0.0f,  0.5f, 0.0f};
@@ -64,11 +121,27 @@ int main(int, char**) {
 
     glBufferData(GL_ARRAY_BUFFER, triangle.size() * sizeof(float), triangle.data(), GL_STATIC_DRAW);
 
+    auto prev = std::chrono::high_resolution_clock::now();
+    auto lag = 0.0;
+
     while (!glfwWindowShouldClose(window))
     {
+        auto curr = std::chrono::high_resolution_clock::now();
+        auto dur = curr - prev;
+        prev = curr;
+        lag += std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+
         glfwPollEvents ();
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+
+        auto model = glm::mat4(1.0);
+        //model = glm::rotate(model, static_cast<float>(lag), glm::vec3(0.0,0.0,1.0));
+        
+        glBufferSubData(GL_UNIFORM, offsetof(UniformBufferObject, model), 16, glm::value_ptr(model));
+
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
